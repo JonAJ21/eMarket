b45003ffdb550977f973be84d66a2607d8418308
JonAJ <jonajmail@gmail.com>
2025-03-25 19:52:29 +0300

    clear.sh

--- clear.sh
+++ clear.sh
@@ -3,9 +3,13 @@
 # Получаем директорию, в которой находится скрипт
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-echo "Начинаем очистку __pycache__ начиная с директории: $SCRIPT_DIR"
 
 # Находим и удаляем все директории __pycache__
 find "$SCRIPT_DIR" -type d -name "__pycache__" -exec rm -rf {} +
 
-echo "Очистка __pycache__ завершена."
+# Удаляем все данный из БД
+
+rm -r .postgres
+rm -r .redis
+
+echo "Очистка завершена."
